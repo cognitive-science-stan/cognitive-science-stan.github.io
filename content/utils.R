e2f7c53f220e215cee97a807e9b9391e159dca8e
bnicenboim <bruno.nicenboim@gmail.com>
2021-10-07 22:29:06 +0200

    datatable

--- content/utils.R
+++ content/utils.R
@@ -1,3 +1,6 @@
+library(dplyr)
+library(purrr)
+library(stringr)
 output <- function(articles){
   endcit<- "</cite></p>"
   begcit<- "<p><cite>"
@@ -17,3 +20,87 @@ output <- function(articles){
     cat(out)
   }
 }
+
+as_nice_tbl <- function(bib) { 
+  # just last names:
+  author <- map_chr(bib, ~ paste0(unlist(.x$author$family), collapse= ", "))
+  bibdf <- as.data.frame(bib) %>%
+    as_tibble()
+  
+  bibdf <- bibdf %>% select(any_of(c("year", "title", "journal", "keywords", "customb", "eprint", "url")))
+  colnames(bibdf)[colnames(bibdf) %in% "customb"] <- "data/code"
+    
+  bibdf <- bibdf %>% #select(year, title, journal, keywords, `data/code` = customb, eprint, doi) %>%
+    mutate(author = author) %>%
+    select(year, author, everything()) %>%
+    arrange(desc(year), author) %>%
+    #removes the NAs that are problematic for DT
+    mutate(across(where(is.character), function(x) ifelse(is.na(x),"",x) )) %>%
+    mutate( title = map_chr(title, function(x){
+      #in brackets with special capitalization    
+      inbrackets  <- str_extract_all(x, "\\{.*?\\}")[[1]] 
+      # changes capitalization
+      x <- str_to_sentence(x)
+      if(length(inbrackets)>0){
+        # puts back the stuff with special capitalization    
+        inbrackets_new  <- str_extract_all(x, "\\{.*?\\}")[[1]]
+        names(inbrackets) <- str_replace(inbrackets_new,"\\{","\\\\{") %>%
+          str_replace("\\}","\\\\}") 
+        x <- stringr::str_replace_all(x,  inbrackets)
+      }
+      x %>% 
+        str_remove_all("\\{") %>%
+        str_remove_all("\\}")
+    })) %>%
+    # add the doi http so that it works as a link
+  #   mutate(doi = ifelse(length(doi)>1 && !str_detect(doi, "https://doi.org/"),
+  #                       paste0("https://doi.org/",doi),
+  #                       doi
+  #   ))
+  # %>%
+    mutate(across(where(is.character), 
+                  function(x) str_replace_all(x,"\\{\\\\&\\}","&")  ))
+  bibdf
+}  
+
+datatable_papers <- function(bibdf){
+  text <- which(colnames(bibdf) %in% c("author", "keywords")) - 1
+  title <- which(colnames(bibdf) %in% c("title"  )) - 1
+  links <- which(colnames(bibdf) %in% c("data/code", "eprint", "doi", "url")) - 1
+  
+datatable(bibdf, rownames = FALSE, options = list(pageLength = 5, 
+                                                        autoWidth = TRUE,
+                                                  columns = list(list(width = "10px"),
+                                                                 list(width = "10px"),
+                                                                 list(width = "200px")),
+                                                        columnDefs = list(
+                                                          list(
+                                                            targets = title,
+                                                            render = JS(
+                                                              "function(data, type, row, meta) {",
+                                                              "return type === 'display' && data.length > 200 ?",
+                                                              "'<span title=\"' + data + '\">' + data.substr(0, 200) + '...</span>' : data;",
+                                                              "}")
+                                                          ),
+                                                          list(
+                                                            targets = text,
+                                                            render = JS(
+                                                              "function(data, type, row, meta) {",
+                                                              "return type === 'display' && data.length > 30 ?",
+                                                              "'<span title=\"' + data + '\">' + data.substr(0, 30) + '...</span>' : data;",
+                                                              "}")
+                                                          ),
+                                                          list(
+                                                            targets =   links,
+                                                            render = JS(
+                                                              "function(data, type, row, meta) {",
+                                                              "return type === 'display' && data.length > 1  ?",
+                                                              "'<a href=\"' + data + '\">' + '[link]' + '</a>' : data;",
+                                                              "}"),
+                                                            width = "5px"
+                                                          )
+                                                        )
+                                                        
+))
+}
+
